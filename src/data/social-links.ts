ab782cfc42d32fec9c754af98040299d2207c29d
Umar Jimoh <umarjimoh@hotmail.com>
2026-02-13 12:22:20 +0100

    Extract reusable social links data

---
+++ src/data/social-links.ts
@@ -0,0 +1,13 @@
+export type SocialLink = {
+  href: string;
+  label: string;
+};
+
+export const socialLinks: SocialLink[] = [
+  {
+    href: "https://www.linkedin.com/company/tekadius",
+    label: "LinkedIn",
+  },
+  { href: "https://www.instagram.com/tekadius", label: "Instagram" },
+  { href: "https://www.x.com/tekadius", label: "Twitter" },
+];
